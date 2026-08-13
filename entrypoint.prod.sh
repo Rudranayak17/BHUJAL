2b9ac9c4bf68b23ba1cb021a8ff8dabf5b60fbf6
radax65-lang <radax65@gmail.com>
2026-08-13 21:40:08 +0530

    Fix Render port configuration

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -6,6 +6,6 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 # Start Gunicorn
-exec gunicorn DWLR.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn DWLR.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers 1
 
 
